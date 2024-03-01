ac531adc72ede8fe1b2b9658f8818cd5c0955911
girmesh1 <girmachewazanaw94@gmail.com>
2024-03-01 10:14:20 -0800

    connect mongodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+//require("dotenv").config();
+
+mongoose.connect('mongodb+srv://shopping:1KhaMCFzvWZWfMnq@cluster0.ojv5is3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('mongodb Connected succesfully!'))
+  .catch(() => {
+    console.log("Mongodb connection faild!")
+  })
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
