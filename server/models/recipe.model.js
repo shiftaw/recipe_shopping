d0f7471a950b298fa55334bebaaaba0873d716ac
girmesh1 <girmachewazanaw94@gmail.com>
2024-03-08 22:42:33 -0800

    correcting the schema

--- server/models/recipe.model.js
+++ server/models/recipe.model.js
@@ -1,17 +1,29 @@
 const mongoose = require("mongoose");
 
 const recipeSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true],
-  },
-  ingridiantes: {
+  Ingredent:
+  {
+    _id:Number,
     name: String,
-    amount: {
-      value: Number,
-      metric: String,
-    },
+    metrics: String,
   },
+
+  Recipe:
+ { 
+   name:String,
+   description: String,
+   metrics: String,
+
+  ingredients: [
+    {
+      id:Number,
+      amount: Number
+    },
+    {
+      id:Number,
+      amount: Number
+    }
+  ]}
 });
 
 module.exports = mongoose.model("recipe.model", recipeSchema);
